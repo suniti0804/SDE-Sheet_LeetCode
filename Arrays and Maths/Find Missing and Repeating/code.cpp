20b99dca1059aee08a0d7f1ea44144035ef871e5
Suniti <72435330+suniti0804@users.noreply.github.com>
2021-08-12 12:28:34 +0530

    Update code.cpp

--- Arrays and Maths/Find Missing and Repeating/code.cpp
+++ Arrays and Maths/Find Missing and Repeating/code.cpp
@@ -1,18 +1,38 @@
- int *findTwoElement(int *A, int len) 
+ int *findTwoElement(int *A, int n) 
     {
-        long long int Sum_N = (len * (len+1) ) /2, Sum_NSq = (len * (len +1) *(2*len +1) )/6;
-        long long int missingNumber=0, repeating=0;
+         long long int n_sum = (n*(n+1))/2, n_sq_sum = (n*(n+1)*(2*n +1))/6;
+         long long int missing = 0, repeating = 0;
 
-        for(int i=0;i<len; i++)
+         for(int i = 0; i < n; i++)
+         {
+            n_sum -= (long long int)A[i];
+            n_sq_sum -= (long long int)A[i]*(long long int)A[i];
+         }
+
+         missing = (n_sum + n_sq_sum/n_sum)/2;
+         repeating = missing - n_sum;
+         long long int* ans = new long long int(2);
+         ans[0] = repeating;
+         ans[1] = missing;
+         return ans;
+    }
+
+
+int *findTwoElement(int *A, int n) 
+    {   
+        int count[n + 1];
+        memset(count, 0, sizeof(count));
+        for(int i = 0; i < n; i++)
+          count[A[i]]++;
+
+        int* ans = new int(2);
+        for(int i = 1; i <= n; i++)
         {
-           Sum_N -= (long long int)A[i];
-           Sum_NSq -= (long long int)A[i]*(long long int)A[i];
+            if(count[i] == 0)
+              ans[1] = i;
+            if(count[i] == 2)
+              ans[0] = i;
         }
 
-        missingNumber = (Sum_N + Sum_NSq/Sum_N)/2;
-        repeating= missingNumber - Sum_N;
-        int* ans = new int(2);
-        ans[0] = repeating;
-        ans[1] = missingNumber;
         return ans;
     }
