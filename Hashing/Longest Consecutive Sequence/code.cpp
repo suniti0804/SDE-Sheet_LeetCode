533f483d2bd2d8a66151498f55afbd62c36428ad
Suniti <72435330+suniti0804@users.noreply.github.com>
2021-08-14 14:33:25 +0530

    Create code.cpp

---
+++ Hashing/Longest Consecutive Sequence/code.cpp
@@ -0,0 +1,30 @@
+int longestConsecutive(vector<int>& arr) 
+    {
+        int N = arr.size();
+        int len = 0, res = INT_MIN;
+        
+        if(N == 0)
+            return 0;
+        unordered_map<int, int> mp;
+        for(int i = 0; i < N; i++)
+           mp[arr[i]]++;
+        
+        for(auto i : arr)
+        {
+            if(mp.find(i - 1) == mp.end())
+            {
+                int k = i;
+                int len = 1;
+                while(mp.find(k + 1) != mp.end())
+                {
+                    k++;
+                    len++;
+                }
+                
+                res = max(res, len);
+            }
+        
+        }
+        return res > 0 ? res : 1;
+        
+    }    
