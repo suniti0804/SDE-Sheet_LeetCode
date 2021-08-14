f1d956469ce562904007f2662398cf090eec1d50
Suniti <72435330+suniti0804@users.noreply.github.com>
2021-08-14 10:31:04 +0530

    Create code.cpp

---
+++ Arrays and Maths/Search a 2D Matrix/code.cpp
@@ -0,0 +1,20 @@
+bool searchMatrix(vector<vector<int>>& matrix, int target) 
+    {
+        int N = matrix.size();
+        int M = matrix[0].size();
+        
+        int s = 0;
+        int e = M - 1;
+        
+        while(s < N && e >= 0)
+        {
+            if(matrix[s][e] == target)
+             return 1;
+            else if(matrix[s][e] > target)
+               e--;
+            else
+              s++;
+        }
+        
+        return 0;
+    }
