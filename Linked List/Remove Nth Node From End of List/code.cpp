8145b1539eb8a432c008809888978c84d6d76af4
Suniti <72435330+suniti0804@users.noreply.github.com>
2021-08-17 19:42:28 +0530

    Create code.cpp

---
+++ Linked List/Remove Nth Node From End of List/code.cpp
@@ -0,0 +1,18 @@
+ListNode* removeNthFromEnd(ListNode* head, int n) 
+    {
+        ListNode *temp = new ListNode(-1);
+        temp -> next = head;
+        ListNode *slow = temp;
+        ListNode *fast = temp;
+        while (n--)
+          fast = fast -> next;
+        
+        while (fast != NULL && fast -> next != NULL) 
+        {
+            fast = fast -> next;
+            slow = slow -> next;
+        }
+        slow -> next = slow -> next -> next;
+        return temp -> next;
+        
+    }
