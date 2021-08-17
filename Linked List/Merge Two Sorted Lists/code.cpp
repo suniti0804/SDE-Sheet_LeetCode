9e2dbb986b7876c92505af65d648015095a730f6
Suniti <72435330+suniti0804@users.noreply.github.com>
2021-08-17 18:34:55 +0530

    Create code.cpp

---
+++ Linked List/Merge Two Sorted Lists/code.cpp
@@ -0,0 +1,43 @@
+ ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) 
+    {
+        ListNode* res = NULL;
+        if(l1 == NULL)
+            return l2;
+        if(l2 == NULL)
+            return l1;
+        
+        if(l1 -> val <= l2 -> val)
+        {
+            res = l1;
+            res -> next = mergeTwoLists(l1 -> next, l2);
+        }
+        
+        else
+        {
+            res = l2;
+            res -> next = mergeTwoLists(l1, l2 -> next);
+        }
+        
+        return res;
+    }
+        
+
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) 
+    {
+        if(l1 == NULL)
+            return l2;
+        if(l2 == NULL)
+            return l1;
+        
+        if(l1 -> val <= l2 -> val)
+        {
+            l1 -> next = mergeTwoLists(l1 -> next, l2);
+            return l1;
+        }
+        
+        else
+        {
+            l2 -> next = mergeTwoLists(l1, l2 -> next);
+            return l2;
+        }
+    }  
